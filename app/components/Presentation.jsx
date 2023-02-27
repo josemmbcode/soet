7d1aa317a7ac26c21740c7c3b217cdd2d2d5a11e
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-02-27 15:11:40 -0600

    improved mobile look

--- app/components/Presentation.jsx
+++ app/components/Presentation.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Presentation() {
   return (
-    <h1 className='text-5xl font-amarillo my-8 text-center'>No compartirlas sería imperdonable </h1>
-  )
+    <>
+      <h1 className="text-5xl font-amarillo my-8 text-center leading-relaxed mx-2 sm:mx-auto">
+        No compartirlas sería imperdonable{" "}
+      </h1>
+      <h2 className="text-2xl my-8 text-center leading-relaxed mx-2 sm:mx-auto">
+        Disponibles de jueves a domingo.
+      </h2>
+    </>
+  );
 }
--- app/components/ProductItem.jsx
+++ app/components/ProductItem.jsx
@@ -3,12 +3,17 @@ import React from "react";
 export default function ProductItem({ product }) {
   return (
     <div className=" m-6 overflow-hidden rounded-lg hover:shadow-slate-500 hover:shadow-md transition-shadow duration-200">
-      <div className="overflow-hidden flex-shrink-0 w-full">
+      <div className="overflow-hidden flex-shrink-0">
         <img className="h-full w-full object-cover" src={product.imgUrl} />
       </div>
-      <div className="flex justify-between p-6 capitalize border-x-2 border-b-2 rounded-b-lg border-soet">
+      <div className="flex justify-between p-3 capitalize border-x-2 border-soet">
         <p>{product.name}</p>
         <h2 className="font-bold text-lg">${product.price}</h2>
+      </div>
+      <div className="border-x-2 border-b-2 rounded-b-lg border-soet flex flex-row justify-end items-center">
+        <button className="p-1 bg-soet rounded-md mr-1 mb-1 text-stone-100">
+          Añadir
+        </button>
       </div>
     </div>
   );
