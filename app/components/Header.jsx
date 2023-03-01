54f2e26c36a98e8a70cc71c95f88b1572f3ad672
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-03-01 15:04:59 -0600

    improved header look and added links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,19 @@
+import { Link } from "@remix-run/react";
 import React from "react";
-
+import { FaShoppingBag } from "react-icons/fa";
 export default function Header() {
   return (
-    <header className="flex justify-between p-6 items-center bg-soet text-sm">
-      <div>Inicio</div>
-      <div>Contactanos</div>
+    <header className="flex justify-between p-3 items-center bg-soet">
+      <Link to="/">
+        <img
+          className="h-24 ml-6"
+          src="/images/donutslogo.svg"
+          alt="soet logo"
+        />
+      </Link>
+      <Link to="/cart">
+        <FaShoppingBag className="text-2xl mr-9" />
+      </Link>
     </header>
   );
 }
