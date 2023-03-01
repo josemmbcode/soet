121121cd39efc59e229de9d52a49c6d970cece48
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-03-01 16:01:58 -0600

    improved cart look

--- app/components/CartItem.jsx
+++ app/components/CartItem.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function CartItem({ item }) {
   return (
-    <div className="flex h-1/3 w-full  border-soet rounded-md border-2 my-3 items-center justify-center">
-      <div className="flex flex-1 h-full justify-center items-center px-2">
+    <div className="flex h-1/3 w-full items-center justify-center">
+      <div className="flex flex-1 h-40 w-40 sm:h-52 sm:w-52 justify-center items-center px-2">
         <img
           src={item.imgUrl}
-          className="h-3/4 border border-black rounded-2xl"
+          className="h-3/4 object-cover border border-black rounded-2xl"
         />
       </div>
       <div className="font-bold flex-1 text-center">{item.name}</div>
+      <div className="font-extrabold flex-1 text-center">${item.price}</div>
       <div className="font-extrabold flex-1 text-center">{item.quantity}</div>
+      <div className="font-extrabold flex-1 text-center">${item.total}</div>
     </div>
   );
 }
--- app/routes/__app/Cart.jsx
+++ app/routes/__app/Cart.jsx
@@ -4,25 +4,39 @@ import CartItem from "../../components/CartItem";
 const DUMMY_CART = [
   {
     id: 1,
-    name: "chocolate",
+    name: "Chocolate",
     price: 12,
     imgUrl: "https://i.ibb.co/Cn5GNXP/unnamed.jpg",
     quantity: 2,
+    total: 24,
   },
   {
     id: 2,
-    name: "flips",
+    name: "Flips",
     price: 12,
     imgUrl: "https://i.ibb.co/wQ01XLB/IMG-8161-1.jpg",
     quantity: 1,
+    total: 12,
   },
 ];
 export default function Cart() {
+  const total = DUMMY_CART.reduce((acc, current) => {
+    return acc + current.total;
+  }, 0);
   return (
-    <div className="flex flex-col items-center mx-3 h-screen sm:mx-32">
+    <div className="flex flex-col items-center justify-start my-4 mx-3 h-auto sm:mx-32 py-0 border border-soet rounded-2xl">
+      <div className="flex w-full mt-3 items-center justify-center  py-1">
+        <div className="flex-1 text-center border-b border-soet" >Foto</div>
+        <div className="flex-1 text-center border-b border-soet">Producto</div>
+        <div className="flex-1 text-center border-b border-soet">Precio</div>
+        <div className="flex-1 text-center border-b border-soet">Cantidad</div>
+        <div className="flex-1 text-center border-b border-soet">Total</div>
+      </div>
       {DUMMY_CART.map((item) => (
         <CartItem key={item.id} item={item} />
       ))}
+          <div className="font-extrabold flex-1 self-end text-center mr-14 p-3">Total:${total}</div>
     </div>
+
   );
 }
