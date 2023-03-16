cd6cf9d676b50c98d76f98b4e6242030443292ec
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-03-16 11:46:47 -0500

    fixed how hours are calculated

--- app/data/utils.server.js
+++ app/data/utils.server.js
@@ -1,16 +1,15 @@
 export function isBusinessOpen() {
   const date = new Date().toLocaleDateString("en-US", {
     timeZone: "America/Caracas",
-    weekday: "long",
-  });
-
-  const hour = new Date().getHours("en-US", {
-    timeZone: "America/Caracas",
+    weekday: "short",
     hour: "numeric",
+    hour12: false,
   });
-  const daysOpen = ["Thursday", "Friday", "Saturday", "Sunday", "Monday"];
 
-  const isOpen = daysOpen.includes(date) && hour >= 14 && hour <= 22;
+  const daysOpen = ["Thu,", "Fri,", "Sat,", "Sun,", "Mon,"];
+  const day = date.split(" ")[0];
+  const hour = +date.split(" ")[1].trim();
+  const isOpen = daysOpen.includes(day) && hour > 13 && hour < 23;
 
   return isOpen;
 }
